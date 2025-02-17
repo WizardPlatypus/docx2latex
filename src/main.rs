fd0228e97840842659135d7a05b6ddab1d7f924a
WizardPlatypus <f.volik.m@gmail.com>
2025-02-17 08:36:55 +0200

    added CLAP
    Signed-off-by: WizardPlatypus <f.volik.m@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+/// Example from the doc
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// A name
+    #[arg(short, long)]
+    name: String,
+    /// A count
+    #[arg(short, long, default_value_t = 1)]
+    count: u8
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    for _ in 0..args.count {
+        println!("Hello {}", args.name);
+    }
 }
