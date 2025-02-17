16e6762e77110d8a914ed275549abe55bcdce294
WizardPlatypus <f.volik.m@gmail.com>
2025-02-17 10:33:48 +0200

    Accepting directory instead of file
    Signed-off-by: WizardPlatypus <f.volik.m@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,40 @@
 use std::path::PathBuf;
 use clap::Parser;
+use std::fs;
+use std::io;
 
 /// A command line utility to convert docx files into latex templates.
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Input docx file to be processed.
+    /// Input directory containing Office Open XML package obtained by unzipping target `.docx` file.
+    /// User is tasked with unzipping the file manually to provide finer control over the filesystem.
     #[arg(short, long)]
     input: PathBuf,
-    /// Output directory, where the resulting latex and media files will be placed.
-    #[arg(short, long)]
-    output: PathBuf
+    // Output directory, where the resulting latex and media files will be placed.
+    // #[arg(short, long)]
+    // output: PathBuf
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     pretty_env_logger::init();
 
     log::info!("Entered 'main'");
-    let args = Args::parse();
 
-    println!("{:?}", args.input);
-    println!("{:?}", args.output);
+    let args = Args::parse();
+    log::debug!("Input directory is {:?}", args.input);
+    // log::debug!("Output directory is {:?}", args.output);
+
+    let mut input = args.input;
+
+    input.push("word");
+    input.push("document.xml");
+
+    log::debug!("Reading {:?}", &input);
+
+    let _document = fs::File::open(&input)?;
 
     log::info!("Exiting 'main'");
+
+    Ok(())
 }
